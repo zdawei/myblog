f566305cd05dafac76eb2810c5251fa918b25a69
zdawei <zdawei1988@gmail.com>
2017-10-29 20:19:42 +0800

    做了登录和注册模块

--- src/js/home/source/template.js
+++ src/js/home/source/template.js
@@ -1,35 +1,29 @@
 var TEMPLATE = {
     loginLayer : 
     '<div class="ui mini modal">'+
-    '<div class="header">'+
-        '用户登录'+
-    '</div>'+
-    '<div class="image content">'+
-        '<form action="" method="get">'+
-            '<p>账号：<input ></p>'+
-            '<p>密码：<input ></p>'+
-        '</form>'+
-    '</div>'+
-    '<div class="actions">'+
-        '<div class="ui button">取消</div>'+
-        '<div class="ui button">确认</div>'+
-    '</div>'+
+        '<div class="header">'+
+            '用户登录'+
+        '</div>'+
+        '<div class="image content">'+
+            '<form action="/aj/login" method="post">'+
+                '<p>账号：<input name="name"></p>'+
+                '<p>密码：<input name="password"></p>'+
+                '<input type="submit" class="ui button">'+
+            '</form>'+
+        '</div>'+
     '</div>',
     registerLayer : 
     '<div class="ui mini modal">'+
-    '<div class="header">'+
-        '用户注册'+
-    '</div>'+
-    '<div class="image content">'+
-        '<form action="" method="get">'+
-            '<p>账号：<input ></p>'+
-            '<p>密码：<input ></p>'+
-        '</form>'+
-    '</div>'+
-    '<div class="actions">'+
-        '<div class="ui button">取消</div>'+
-        '<div class="ui button">确认</div>'+
-    '</div>'+
+        '<div class="header">'+
+            '用户注册'+
+        '</div>'+
+        '<div class="image content">'+
+            '<form action="/aj/register" method="post">'+
+                '<p>账号：<input name="name"></p>'+
+                '<p>密码：<input name="password"></p>'+
+                '<input type="submit" class="ui button">'+
+            '</form>'+
+        '</div>'+
     '</div>'
 };
 
